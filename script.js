8cab2c04ca6bb3111f9c58ffe3186a240789013c
Michael3682 <152974233+Michael3682@users.noreply.github.com>
2024-09-07 03:40:39 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,61 +1,44 @@
 const animate1 = () => {
     var tl = gsap.timeline()
 
-    tl.from("nav h1, a", {
-        y: -30,
-        opacity: 0,
-        delay: .3,
-        duration: .6,
-        stagger: .15
-    })
-    tl.from("button", {
-        x: 50,
-        opacity: 0
-    })
+    tl.from("nav", { y: -100, duration: .7, delay: .5 })
+    tl.from("nav h1", { opacity: 0, delay: .5, duration: .5, stagger: .15 })
+    tl.from("nav a", { y: -30, opacity: 0, duration: .5, stagger: .15 })
+    tl.from("nav button", { x: 50, opacity: 0 })
 }
-
 const buttonsHoverAnimation = () => {
     const buttons = gsap.utils.toArray(".btn");
+    const btn = document.querySelector("#btn")
+    const btn2 = document.querySelector("#btn2")
+
+    var tl = gsap.timeline({ paused: true })
+    var tl2 = gsap.timeline({ paused: true })
+    var tl3 = gsap.timeline({ paused: true })
+
     buttons.forEach((item) => {
         let element = item.querySelector("span");
         var tl = gsap.timeline({ paused: true })
-
-        tl.to(element, {
-            backgroundColor: "#8b8b837a",
-            duration: .2,
-        })
-
+        tl.to(element, { backgroundColor: "#8f4c1299", duration: .15, })
         item.addEventListener("mouseenter", () => tl.play())
         item.addEventListener("mouseleave", () => tl.reverse())
     });
 
-    const btn = document.querySelector("#btn")
-    var tl = gsap.timeline({ paused: true })
-
-    tl.to("nav button", {
-        backgroundColor: "transparent",
-        color: "#4e342e"
-    })
+    tl.to("nav button", { backgroundColor: "transparent", color: "#fdca6f", duration: .3 })
+    tl2.to(".parts #btn2", { backgroundColor: "transparent", color: "#fdca6f" })
+    tl3.to("#icon", { duration: .2, yPercent: -150, ease: "power2.in" });
+    tl3.set("#icon", { yPercent: 150 });
+    tl3.to("#icon", { duration: .2, yPercent: 0 });
 
     btn.addEventListener("mouseenter", () => tl.play())
     btn.addEventListener("mouseleave", () => tl.reverse())
+    btn2.addEventListener("mouseenter", () => { tl2.play(), tl3.play() })
+    btn2.addEventListener("mouseleave", () => { tl2.reverse(), tl3.reverse() })
+    btn2.addEventListener("click", () => document.querySelector("#section1").scrollIntoView({ behavior: 'smooth'}))
 }
 
-const animate2 = () => {
-    gsap.to("#about h1", {
-        transform: "translateX(-55%)",
-        scrollTrigger: {
-            trigger: "#about",
-            scroller: "body",
-            scrub: 1,
-            start: "top 0",
-            end: "top -200%",
-            pin: true,
-            markers: true
-        }
-    })
-}
+const home = document.querySelector("#home")
+home.addEventListener("click", () => document.querySelector("body").scrollIntoView({ behavior: 'smooth' }))
 
-buttonsHoverAnimation()
+
 animate1()
-animate2()
+buttonsHoverAnimation()
